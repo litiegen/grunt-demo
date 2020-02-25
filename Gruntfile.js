6e0cd71cbc93517f60d70af4accc133d1dabcd14
haddis69 <1474154683@qq.com>
2020-02-25 15:18:30 +0800

    htmlhint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,14 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        less: {
-            compile: {
-                files: { 'dist/compiled.css': 'css/*.less' }
-            }
-        }
-    });
-    
-    grunt.loadNpmTasks('grunt-contrib-less');
+		htmlhint: {
+			options: {
+				htmlhintrc: '.htmlhintrc'
+			},
+			src: ['*.html']
+		}
+	});
   
-    grunt.registerTask('default', ['less:compile']);
-};
+    grunt.loadNpmTasks('grunt-htmlhint');
+  
+    grunt.registerTask('default', ['htmlhint']);
+  };
