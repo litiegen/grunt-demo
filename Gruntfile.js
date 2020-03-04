f4a4af603cae9f6bdd950118eae8c3b7e001f7b2
litiegen <517206296@qq.com>
2020-03-04 08:17:55 +0800

    image压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,15 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-	  run: {
-		api: {
-		  options: { wait: false },
-		  args: ['./app.js']
-		}
-	  },
-	  mochacli: {
-		options: {
-		  reporter: 'spec',
-		  bail: true
-		},
-		  all: ['test/*.js']
-	  }
-	});
-  
-	grunt.loadNpmTasks('grunt-run')
-	grunt.loadNpmTasks('grunt-mocha-cli')
-  
-	grunt.registerTask('default', ['run', 'mochacli', 'stop:api']);
-  };
+  grunt.initConfig({
+    imagemin: {                               
+      files: {
+        expand: true,
+        src: ['images/*.{png,jpg,gif}'],
+        dest: 'myimg/'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  grunt.registerTask('default', ['imagemin']);    
+};
